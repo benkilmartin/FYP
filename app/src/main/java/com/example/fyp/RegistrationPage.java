abc3d9d4b2c99fcb40b8ae02a2bd77f45612f9df
benkilmartin <benkilmartin62@gmail.com>
2022-07-28 13:36:20 -0400

    fininshed signup and login UI

--- app/src/main/java/com/example/fyp/RegistrationPage.java
+++ app/src/main/java/com/example/fyp/RegistrationPage.java
@@ -8,6 +8,7 @@ public class RegistrationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
     }
